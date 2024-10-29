17650c0bc8e6e9c7ed4318a10b17c88c0cd08dbe
Ruwei 马 <ruweima86@gmail.com>
2024-10-29 01:42:09 -0400

    update read.me

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SCL Project
+
+## Get Started
+
+### Install node dependencies
+cd map && npm install
+
+### Run visulization
+
+npm start
+
+## 2d - Demos
+Embedding, Clustering & 2D viz is in jupyter notebooks.
+To set up environment, at root directory, run 
+
+poetry install.
--- map/src/App.js
+++ map/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import ClusterMap from './components/ClusterMap'; // Assuming your ClusterMap component renders the objects
+import ClusterMap from './components/ClusterMap';
 
 function App() {
   return (
     <div className="App">
       <Canvas>
-        {/* Lighting */}
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        {/* Render ClusterMap component */}
         <ClusterMap />
       </Canvas>
     </div>
